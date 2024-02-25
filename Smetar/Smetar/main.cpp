76040fd32ac51d6160f62a44496de32b12a9fadb
martinoprin <martinoprin8@gmail.com>
2024-02-25 13:10:08 +0100

    Game.h added

--- Smetar/Smetar/main.cpp
+++ Smetar/Smetar/main.cpp
@@ -1,15 +1,19 @@
-#include "SDL.h"
+#include "Game.h"
+
+Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, 0);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 0, 176, 200, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(10000);
+	game = new Game();
+	game->init("GameWindow", 800, 600, false);
 
+	while (game->running())
+	{
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+
+	game->clean();
 	return 0;
 }
-
